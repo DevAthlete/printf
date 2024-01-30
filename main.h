571c5618a221a9cf9f600f2d3a3b578d33df531c
Azeddine chakir <azeddine.chakir@um5r.ac.ma>
2024-01-30 12:37:47 -0800

    adding p_cha p_str functions

--- main.h
+++ main.h
@@ -6,9 +6,9 @@
 #include <unistd.h>
 
 int _putchar(char c);
-int cus_print_char(va_list argz);
-int print_strings(va_list velles);
-int _str_len(char *str);
+int p_cha(va_list argz);
+int p_string(va_list velles);
+int _strlenth(char *str);
 int _str_lenc(const char *string);
 int print_perc(void);
 int _printf(const char *format, ...);
