44239790ffb7a95e6fc926834b356c6fd7be5cf1
DevAthlete <youssef.oumouni@um5r.ac.ma>
2024-01-30 16:06:12 -0800

    ptr modyvi main.h

---
+++ get_mod.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_mody - set all needed modifiers
+ * @chr: modifier to set
+ * @mody: pointer to modifiers
+ *
+ * Return: 1 if modifier set
+ * 0 Otherwise
+ */
+int get_mody(char chr, mod_t *mody)
+{
+	(void) chr;
+	(void) mody;
+	return (0);
+}
+
--- main.h
+++ main.h
@@ -22,5 +22,6 @@ int addn_HEX(unsigned int numbers);
 int p_s_enc(va_list ls_args);
 int addn_hex(unsigned long int numbers);
 int HEX(va_list velo);
+int get_mody(char chr, mod_t *mody);
 
 #endif
