7e796506d542df517a216c7ade7721fa2c8aea91
DevAthlete <youssef.oumouni@um5r.ac.ma>
2024-01-30 17:16:11 -0800

    fun

--- main.h
+++ main.h
@@ -1,9 +1,67 @@
-#ifndef MAIN_H
-#define MAIN_H
-#define BUFFER_SIZE 1024
-#include <stdarg.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <limits.h>
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include <unistd.h>
+
+#define NULL_STR "(null)"
+#define BUFFER_SIZE 1024
+
+/**
+ * struct flags - struct for flags
+ * @plus: flag for '+' char
+ * @minus: flag for '-' char
+ * @zero: flag for '0' char
+ * @space: flag for ' ' char
+ * @hash: flag for '#' char
+ *
+ * flag struct is to store flags for specifiers/identifiers
+ * flag set to 1 when used, otherwise 0.
+ */
+
+typedef struct flags
+{
+	int plus;
+	int minus;
+	int zero;
+	int space;
+	int hash;
+} flags_t;
+
+/**
+ * struct modifiers - struct for modifiers
+ * @len_l: length modifier for the 'l' char
+ * @len_h: length modifier for the 'h' char
+ * @wdth: min width to print
+ * @prec: precision of number
+ *
+ * modifier struct to store all modifiers for the specifier
+ * modifier set to 1 whenused, otherwise 0.
+ */
+typedef struct modifiers
+{
+	int len_l;
+	int len_h;
+	int wdth;
+	int prec;
+} mod_t;
+
+/**
+ * struct format - converter printf
+ * @ph: type char pointer of specifier
+ * @func: function for conversion specifier
+ *
+ */
+
+typedef struct format
+{
+	char *ph;
+	int (*func)();
+} convert;
 
 int _putchar(char c);
 int p_cha(va_list argz);
@@ -30,4 +88,5 @@ int p_hex(va_list var);
 int get_mody(char chr, mod_t *mody);
 int p_bin(va_list var);
 int flags(char chr, flags_t *flgs)
+
 #endif
